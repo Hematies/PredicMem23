0409e97f81c1c28817b9476721e67edd688995e5
Pablo <pablo-aries-7@hotmail.com>
2024-07-15 12:18:31 +0200

    Añadidas licencias GNU

--- PredicMem23/Simulator.h
+++ PredicMem23/Simulator.h
@@ -1,3 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//    Copyright (c) 2024  Pablo Sánchez Cuevas                    //
+//                                                                             //
+//    This file is part of PredicMem23.                                            //
+//                                                                             //
+//    PredicMem23 is free software: you can redistribute it and/or modify          //
+//    it under the terms of the GNU General Public License as published by     //
+//    the Free Software Foundation, either version 3 of the License, or        //
+//    (at your option) any later version.                                      //
+//                                                                             //
+//    PredicMem23 is distributed in the hope that it will be useful,               //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the              //
+//    GNU General Public License for more details.                             //
+//                                                                             //
+//    You should have received a copy of the GNU General Public License        //
+//    along with PredicMem23. If not, see <
+// http://www.gnu.org/licenses/>.
+//
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 template<typename T, typename I, typename A, typename LA>
 class Simulator {
